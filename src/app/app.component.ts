854970f14a17837f9d4cca0befcf49438ee66045
M0rfes <fahimkhan20148@gmail.com>
2019-09-18 18:50:33 +0530

    started working on chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    private authS: AuthService,
+    private navCon: NavController,
   ) {
     this.initializeApp();
+  }
+  ngOnInit(): void {
+    this.authS.user.subscribe(user => {
+      if (user) {
+        this.navCon.navigateRoot('/chat');
+      } else {
+        this.navCon.navigateRoot('/');
+      }
+    });
   }
 
   initializeApp() {
--- src/app/chat/channel/channel.page.ts
+++ src/app/chat/channel/channel.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-channel',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./channel.page.scss'],
 })
 export class ChannelPage implements OnInit {
+  constructor(public authS: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
